289bf075729796809715ef66356df00d9e5f7723
Boyd Johnson <johnson.boyd@gmail.com>
2019-09-02 03:15:30 -0500

    Add components common to several commands

--- src/ndjson.rs
+++ src/ndjson.rs
@@ -1,35 +1,45 @@
-extern crate geo;
-extern crate geojson;
-
 use crate::error::NdJsonSpatialError;
 use geojson::GeoJson;
-use std::io::Write;
+use serde_json::Value;
+use std::io::{stderr, stdin, BufRead, BufReader, Stdin, Write};
 
-pub struct NdJsonReader {}
+/// A struct for reading ndjson geojson from a BufRead source,
+/// most usually BufReader<Stdin> which is provided by NdjsonGeojsonReader::default().
+/// Often used as the Iterator impl for which `Self::Item = geojson::GeoJson`.
+pub struct NdJsonGeojsonReader<IN> {
+    std_in: IN,
+}
 
-impl NdJsonReader {
-    pub fn new() -> Self {
-        NdJsonReader {}
+impl<IN> NdJsonGeojsonReader<IN>
+where
+    IN: BufRead,
+{
+    pub fn new(std_in: IN) -> Self {
+        NdJsonGeojsonReader { std_in }
     }
 }
 
-impl Default for NdJsonReader {
+impl<'a> Default for NdJsonGeojsonReader<BufReader<Stdin>> {
     fn default() -> Self {
-        Self::new()
+        let s = stdin();
+        Self::new(BufReader::new(s))
     }
 }
 
-impl Iterator for NdJsonReader {
+impl<IN> Iterator for NdJsonGeojsonReader<IN>
+where
+    IN: BufRead,
+{
     type Item = Result<GeoJson, NdJsonSpatialError>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut line = String::new();
-        match ::std::io::stdin().read_line(&mut line) {
+        match self.std_in.read_line(&mut line) {
             Ok(_) => {
                 if line.is_empty() {
                     return None;
                 }
-                let line = if line.ends_with(",") {
+                let line = if line.ends_with(',') {
                     let comma_index = line.len() - 1;
                     line[..comma_index].to_string()
                 } else {
@@ -41,14 +51,62 @@ impl Iterator for NdJsonReader {
                 )
             }
             Err(e) => {
-                let error_msg = format!("Error reading ndjson: {}", e);
+                writeln!(stderr(), "Error reading ndjson: {}", e)
+                    .expect("Unable to write to stderr");
 
-                if !::std::io::stderr()
-                    .write(error_msg.as_bytes().as_ref())
-                    .is_ok()
-                {
-                    panic!("Error writing to stderr: {}", error_msg);
+                None
+            }
+        }
+    }
+}
+
+/// Reads json items from a ndjson BufRead.
+/// The Default impl gives you the source of
+/// `BufReader<Stdin>`. Usually used as the Iterator impl,
+/// for which `Self::Item = serde_json::Value`.
+pub struct NdjsonReader<IN> {
+    std_in: IN,
+}
+
+impl<IN> NdjsonReader<IN> {
+    pub fn new(std_in: IN) -> Self {
+        NdjsonReader { std_in }
+    }
+}
+
+impl<'a> Default for NdjsonReader<BufReader<Stdin>> {
+    fn default() -> Self {
+        Self::new(BufReader::new(stdin()))
+    }
+}
+
+impl<IN> Iterator for NdjsonReader<IN>
+where
+    IN: BufRead,
+{
+    type Item = Result<Value, NdJsonSpatialError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut line = String::new();
+        match self.std_in.read_line(&mut line) {
+            Ok(_) => {
+                if line.is_empty() {
+                    return None;
                 }
+                let line = if line.ends_with(',') {
+                    let comma_index = line.len() - 1;
+                    line[..comma_index].to_string()
+                } else {
+                    line
+                };
+                Some(
+                    serde_json::from_str(&line)
+                        .map_err(|e| NdJsonSpatialError::Error(format!("{:?}", e))),
+                )
+            }
+            Err(e) => {
+                writeln!(stderr(), "Error reading ndjson: {}", e)
+                    .expect("Unable to write to stderr");
                 None
             }
         }
